f5abd16901b9cf177346e5ff64b2df8895371244
Manuel Guerra <57271488+manuwarfare@users.noreply.github.com>
2024-05-08 23:32:43 -0400

    Update launcher.sh

--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -1,1 +1,18 @@
+#!/usr/bin/env bash
+if [ $# -ne 0 ];then
+    echo "I have no parameters just run the script without arguments"
+    exit 1
+fi
 
+notify-send "Photoshop CC" "Photoshop CC launched." -i "photoshopicon"
+
+SCR_PATH="pspath"
+CACHE_PATH="pscache"
+
+RESOURCES_PATH="$SCR_PATH/resources"
+WINE_PREFIX="$SCR_PATH/prefix"
+ 
+
+export WINEPREFIX="$WINE_PREFIX"
+
+wine64 "$SCR_PATH/prefix/drive_c/users/$USER/PhotoshopSE/Photoshop.exe"
